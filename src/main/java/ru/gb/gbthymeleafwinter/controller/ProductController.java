9b7ecadef0753301d1b1700eec9f1e50065f1d06
PSelyutin <pselyutin@inno.tech>
2022-03-15 09:16:20 +0400

    init commit

---
+++ src/main/java/ru/gb/gbthymeleafwinter/controller/ProductController.java
@@ -0,0 +1,51 @@
+package ru.gb.gbthymeleafwinter.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.gb.gbthymeleafwinter.entity.Product;
+import ru.gb.gbthymeleafwinter.service.ProductService;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/product")
+public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping("/all")
+    public String getProductList(Model model) {
+        model.addAttribute("products", productService.findAll());
+        return "product-list";
+    }
+
+    @GetMapping
+    public String showForm(Model model, @RequestParam(name = "id", required = false) Long id) {
+        Product product;
+
+        if (id != null) {
+            product = productService.findById(id);
+        } else {
+            product = new Product();
+        }
+        model.addAttribute("product", product);
+        return "product-form";
+    }
+
+    @PostMapping
+    public String saveProduct(Product product) {
+        productService.save(product);
+        return "redirect:/product/all";
+    }
+
+    @GetMapping("/delete")
+    public String deleteById(@RequestParam(name = "id") Long id) {
+        productService.deleteById(id);
+        return "redirect:/product/all";
+    }
+
+}
